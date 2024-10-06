a2f9c5d9d0b8042a0e62a36a06521882ab5f776a
TejasOhlan <tehjash.ohlaan@geminisolutions.com>
2024-10-06 13:14:31 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Calender from "./components/Calender";
 
 function App() {
-  return <>hi</>;
+  return (
+    <>
+      <Calender />
+    </>
+  );
 }
 
 export default App;
